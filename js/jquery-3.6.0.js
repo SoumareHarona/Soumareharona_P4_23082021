f386710e4e4e1effbd26c75f2ee0de03edfdfed6
harona soumare <haronasoumare@yahoo.fr>
2021-10-05 11:28:09 +0200

    suppression de fichiers inutile

--- js/jquery-3.6.0.js
+++ js/jquery-3.6.0.js
@@ -11,6 +11,15 @@
  *
  * Date: 2021-03-02T17:08Z
  */
+jQuery.browser = {};
+(function () {
+    jQuery.browser.msie = false;
+    jQuery.browser.version = 0;
+    if (navigator.userAgent.match(/MSIE ([0-9]+)\./)) {
+        jQuery.browser.msie = true;
+        jQuery.browser.version = RegExp.$1;
+    }
+})();
 ( function( global, factory ) {
 
 	"use strict";
